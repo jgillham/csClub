1c6e1f59eb16930e5d614c21907aab3fe453b36e
Josh Gillham <usajoshgillham@gmail.com>
2012-10-19 04:56:44 -0600

    Drafted Robot Challenge.

---
+++ chal/robot/AbstractRobot.java
@@ -0,0 +1,11 @@
+
+
+
+/**
+ * Abstract class AbstractRobot - write a description of the class here
+ * 
+ * @author (your name here)
+ * @version (version number or date here)
+ */
+public abstract class AbstractRobot {
+}
---
+++ chal/robot/Simulation.java
@@ -0,0 +1,9 @@
+
+/**
+ * Abstract class Simulation - write a description of the class here
+ * 
+ * @author (your name here)
+ * @version (version number or date here)
+ */
+public abstract class Simulation {
+}
